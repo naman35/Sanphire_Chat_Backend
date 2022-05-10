978cc3c833c94c5e58d1356ecd6c879364a41f1b
Naman Agarwal <agarwalnaman35@gmail.com>
2022-05-10 19:20:17 +0530

    set avatar module

--- controllers/userController.js
+++ controllers/userController.js
@@ -4,6 +4,27 @@ const bcrypt = require("bcrypt");
 
 
 
+module.exports.setAvatar = async (req, res, next) => {
+  try {
+    const userId = req.params.id;
+    const avatarImage = req.body.image;
+    const userData = await User.findByIdAndUpdate(
+      userId,
+      {
+        isAvatarImageSet: true,
+        avatarImage,
+      },
+      { new: true }
+    );
+    return res.json({
+      isSet: userData.isAvatarImageSet,
+      image: userData.avatarImage,
+    });
+  } catch (ex) {
+    next(ex);
+  }
+};
+
 module.exports.logOut = (req, res, next) => {
   try {
     if (!req.params.id) return res.json({ msg: "User id is required " });
