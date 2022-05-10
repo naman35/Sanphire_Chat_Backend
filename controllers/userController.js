5a7521a493c8753013d2a48ff128ce88bbd45c46
Naman Agarwal <agarwalnaman35@gmail.com>
2022-05-10 19:20:41 +0530

    get all users module

--- controllers/userController.js
+++ controllers/userController.js
@@ -3,6 +3,19 @@ const bcrypt = require("bcrypt");
 
 
 
+module.exports.getAllUsers = async (req, res, next) => {
+  try {
+    const users = await User.find({ _id: { $ne: req.params.id } }).select([
+      "email",
+      "username",
+      "avatarImage",
+      "_id",
+    ]);
+    return res.json(users);
+  } catch (ex) {
+    next(ex);
+  }
+};
 
 module.exports.setAvatar = async (req, res, next) => {
   try {
